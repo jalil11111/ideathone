d2a79ad90de7cb95513a9a329024ba31428331e6
jalil benzid <ahmed.jalil0111@gmail.com>
2024-11-11 20:24:51 +0100

    add

--- icon.js
+++ icon.js
@@ -1,11 +1,12 @@
-document.getElementById("icn"),addEventListener("click",display_func);
+
+document.getElementById("icn").addEventListener("click",display_func);
 
 function display_func(){
     let cnt = document.getElementById("ourul");
     cnt.style.display = 'flex';
     setTimeout(function(){
         cnt.style.opacity = 1;
-        cnt.style.height = "1800px"
+        cnt.style.height = "150%";
     },10)
 
     let btn = document.getElementById("icn");
@@ -13,6 +14,15 @@ function display_func(){
 
     let ev = document.getElementById("every_thing");
     ev.style.opacity = 0.7;
+
+    let li = document.getElementById("lil1");
+    li.style.display = 'flex';
+    let li1 = document.getElementById("lil2");
+    li1.style.display = 'flex';
+    let li2 = document.getElementById("lil3");
+    li2.style.display = 'flex';
+    let li3 = document.getElementById("lil4");
+    li3.style.display = 'flex';
 
     let hd = document.getElementById("logo");
     hd.style.opacity = 0.9;
