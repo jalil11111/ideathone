29021a765e36757cda1a0cdf2c8ef5c8bcd477f6
jalil benzid <ahmed.jalil0111@gmail.com>
2024-11-14 21:30:52 +0100

    add

---
+++ display.js
@@ -0,0 +1,89 @@
+function display1(){
+    let d1 = document.getElementById("des-1")
+    d1.style.display = 'block';
+    let h1 = document.getElementById("hide-1")
+    h1.style.display = 'flex'
+    let b1 = document.getElementById("display-1")
+    b1.style.display = 'none'
+}
+
+function hide1(){
+    let d1 = document.getElementById("des-1")
+    d1.style.display = 'none';
+    let h1 = document.getElementById("hide-1")
+    h1.style.display = 'none'
+    let b1 = document.getElementById("display-1")
+    b1.style.display = 'block'
+}
+
+function display2(){
+    let d2 = document.getElementById("des-2")
+    d2.style.display = 'block';
+    let h2 = document.getElementById("hide-2")
+    h2.style.display = 'flex'
+    let b2 = document.getElementById("display-2")
+    b2.style.display = 'none'
+}
+
+function hide2(){
+    let d2 = document.getElementById("des-2")
+    d2.style.display = 'none';
+    let h2 = document.getElementById("hide-2")
+    h2.style.display = 'none'
+    let b2 = document.getElementById("display-2")
+    b2.style.display = 'block'
+}
+
+function display3(){
+    let d3 = document.getElementById("des-3")
+    d3.style.display = 'block';
+    let h3 = document.getElementById("hide-3")
+    h3.style.display = 'flex'
+    let b3 = document.getElementById("display-3")
+    b3.style.display = 'none'
+}
+
+function hide3(){
+    let d3 = document.getElementById("des-3")
+    d3.style.display = 'none';
+    let h3 = document.getElementById("hide-3")
+    h3.style.display = 'none'
+    let b3 = document.getElementById("display-3")
+    b3.style.display = 'block'
+}
+
+function display4(){
+    let d4 = document.getElementById("des-4")
+    d4.style.display = 'block';
+    let h4 = document.getElementById("hide-4")
+    h4.style.display = 'flex'
+    let b4 = document.getElementById("display-4")
+    b4.style.display = 'none'
+}
+
+function hide4(){
+    let d4 = document.getElementById("des-4")
+    d4.style.display = 'none';
+    let h4 = document.getElementById("hide-4")
+    h4.style.display = 'none'
+    let b4 = document.getElementById("display-4")
+    b4.style.display = 'block'
+}
+
+function display5() {
+    let d5 = document.getElementById("des-5");
+    d5.style.display = 'block';
+    let h5 = document.getElementById("hide-5");
+    h5.style.display = 'flex';
+    let b5 = document.getElementById("display-5");
+    b5.style.display = 'none';
+}
+
+function hide5() {
+    let d5 = document.getElementById("des-5");
+    d5.style.display = 'none';
+    let h5 = document.getElementById("hide-5");
+    h5.style.display = 'none';
+    let b5 = document.getElementById("display-5");
+    b5.style.display = 'block';
+}
